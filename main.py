8003d6326638c1719dba63674933252ac5f007e1
Giovane Bianchi Milani <80916741+giovanebmilani@users.noreply.github.com>
2021-09-21 00:14:50 -0300

    Trying something

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
+import requests
+import json
 
 
+
+def upload(filename):
+    pass
+
+
+def authenticate():
+    
+    url = "https://www.strava.com/oauth/authorize"
+
+    data = {}
+    data["client_secret"] = 69535
+    data["redirect_uri"] = "developers.strava.com"
+    data["response_type"] = "code"
+    data["scope"] = "activity:write"
+
+    response = requests.get(url, data=data)
+    return response
+
+
+def token_exchange():
+    pass
+
+
+
+print(authenticate().json())
