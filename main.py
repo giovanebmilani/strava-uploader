b0cd3ec3d4d5a4077ec1ffc45f8c5dafaae540d4
Giovane Bianchi Milani <80916741+giovanebmilani@users.noreply.github.com>
2022-02-09 16:54:04 -0300

    Uploading in working

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import webbrowser
 client_id = '69535'
 client_secret = 'bac246d8c108ae3a6f7682be186105d513e33727'
 redirect_uri = 'http://127.0.0.1:5000/authorized'
-scopes = 'activity:write,read_all'
+scopes = 'activity:write,read_all,activity:read_all'
 
 auth_url = 'http://www.strava.com/oauth/authorize?client_id='+client_id+'&response_type=code&redirect_uri='+redirect_uri+'&approval_prompt=force&scope='+scopes
 
